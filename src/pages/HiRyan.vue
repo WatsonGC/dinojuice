8f07c028d118217f72565f1a4ca9be88ca422d69
Gabe Watson <gwatson@quiktrip.com>
2021-09-23 07:39:26 -0500

    pick

--- src/pages/HiRyan.vue
+++ src/pages/HiRyan.vue
@@ -1,8 +1,12 @@
 <template>
   <q-page>
-    <div style="font-size: 18vh" class="text-centered">
+    <!-- <div style="font-size: 18vh" class="text-centered">
 {{message}}
-</div>
+</div> -->
+      <div class="row">
+      <q-input v-model="loadingDate" stack-label filled type="date" label="Loading date" style="max-width:250px;min-width:250px;" class="q-pa-sm"/>
+      <q-input v-model="loadingTime" stack-label filled type="time" label="Loading time" style="max-width:250px;min-width:250px;" class="q-pa-sm"/>
+      </div>
   </q-page>
 </template>
 
@@ -11,7 +15,9 @@ export default {
   name: 'HiRyan',
   data () {
     return {
-      message: 'Hi Ryan!'
+      message: 'Hi Ryan!',
+      loadingDate: '',
+      loadingTime: ''
     };
   },
 };
